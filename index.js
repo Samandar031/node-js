a48d19606a721cfc1219b7e1ff53c8b125327601
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-04-19 18:05:30 +0500

    a

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("urre server ishladi");
+});
+
+server.listen("8000", "127.0.0.1");
 
 // const inputText = fs.readFileSync("./txt/input.txt", "utf-8");
 
@@ -9,8 +16,13 @@ const fs = require("fs");
 // fs.writeFileSync("./txt/output.txt", outputTxt);
 // console.log("yozib bo'ldim");
 
-fs.readFile("./txt/input.txt", "utf-8", function (err, data) {
-  console.log(err);
-  console.log(data);
-});
-console.log("Reading...");
+// fs.readFile("./txt/input.txt", "utf-8", function (err, data) {
+//   console.log(err);
+//   console.log(data);
+// });
+// console.log("Reading...");
+
+// fs.writeFile("output.txt", "salom", "utf-8", function (err) {
+//   if (err) throw new Error("xato");
+//   console.log("filega malumotlarni yozildi");
+// });
