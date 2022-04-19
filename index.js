cdf7347235d361d772e8ba98c97421aa73155fab
Samandar Qodirqulov <samandarqodirqulov021@gmail.com>
2022-04-19 16:55:08 +0500

    fs

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const fs = require("fs");
 
-const inputText = fs.readFileSync("./txt/input.txt", "utf-8");
+// const inputText = fs.readFileSync("./txt/input.txt", "utf-8");
 
-// console.log(inputText);
+// // console.log(inputText);
 
-const outputTxt = `salom qalay mutantlar: ${inputText}`;
+// const outputTxt = `salom qalay mutantlar: ${inputText}`;
 
-fs.writeFileSync("./txt/output.txt", outputTxt);
-console.log("yozib bo'ldim");
+// fs.writeFileSync("./txt/output.txt", outputTxt);
+// console.log("yozib bo'ldim");
+
+fs.readFile("./txt/input.txt", "utf-8", function (err, data) {
+  console.log(err);
+  console.log(data);
+});
+console.log("Reading...");
